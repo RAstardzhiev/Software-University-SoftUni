058ab5c95a52749772f16b70d719f3ae19614584
RAstardzhiev <freeccqeb@gmail.com>
2017-01-27 12:25:22 +0200

    Ready with Methods and Debugging - Exercises

---
+++ Programming Fundamentals/Methods and Debugging/Debugging Instruction Set/Instruction_Set.cs
@@ -0,0 +1,54 @@
+﻿namespace Debugging_Instruction_Set
+{
+    using System;
+
+    public class Instruction_Set
+    {
+        public static void Main()
+        {
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                string[] codeArgs = command.Split(' ');
+
+                long result = 0;
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne + 1;
+                            break;
+                        }
+
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne - 1;
+                            break;
+                        }
+
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne * operandTwo;
+                            break;
+                        }
+                }
+
+                Console.WriteLine(result);
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Methods and Debugging/Sequence of Commands/BrokenToFix.cs
@@ -0,0 +1,74 @@
+﻿namespace Sequence_of_Commands
+{
+    using System;
+    using System.Linq;
+
+    public class BrokenToFix
+    {
+        private const char ArgumentsDelimiter = ' ';
+
+        public static void Main()
+        {
+            int sizeOfArray = int.Parse(Console.ReadLine());
+
+            long[] array = Console.ReadLine()
+                .Trim()
+                .Split(ArgumentsDelimiter)
+                .Select(long.Parse)
+                .ToArray();
+
+            string[] command = Console.ReadLine().Split(ArgumentsDelimiter);
+
+            while (!command[0].Equals("stop"))
+            {
+                PerformAction(array, command);
+                Console.WriteLine(string.Join(" ", array));
+                command = Console.ReadLine().Split(ArgumentsDelimiter);
+            }
+        }
+
+        public static void PerformAction(long[] arr, string[] comand)
+        {
+            switch (comand[0].ToLower())
+            {
+                case "multiply":
+                    arr[int.Parse(comand[1]) - 1] *= long.Parse(comand[2]);
+                    break;
+                case "add":
+                    arr[int.Parse(comand[1]) - 1] += long.Parse(comand[2]);
+                    break;
+                case "subtract":
+                    arr[int.Parse(comand[1]) - 1] -= long.Parse(comand[2]);
+                    break;
+                case "lshift":
+                    ArrayShiftLeft(arr);
+                    break;
+                case "rshift":
+                    ArrayShiftRight(arr);
+                    break;
+            }
+        }
+
+        public static void ArrayShiftRight(long[] array)
+        {
+            long temp = array[array.Length - 1];
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = temp;
+        }
+
+        public static void ArrayShiftLeft(long[] array)
+        {
+            long temp = array[0];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[array.Length - 1] = temp;
+        }
+    }
+}
