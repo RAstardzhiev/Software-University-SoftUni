0cac864d59edd346733d1d76059d7b2df2ff3f1f
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-02-18 15:56:34 +0200

    Add "Query Mess" to "Regex - Exercise"

---
+++ Programming Fundamentals/Regex - Exercise/Query Mess/QueryMess.cs
@@ -0,0 +1,58 @@
+﻿namespace Query_Mess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class QueryMess
+    {
+        public static void Main(string[] args)
+        {
+            string query = Console.ReadLine();
+
+            while (!query.ToLower().Equals("end"))
+            {
+                Dictionary<string, List<string>> fieldValue = new Dictionary<string, List<string>>();
+                ExtractQueryData(fieldValue, query.Split('&'));
+
+                Console.WriteLine(string.Join(string.Empty, fieldValue
+                    .Select(x => $"{x.Key}=[{string.Join(", ", x.Value)}]")));
+
+                query = Console.ReadLine();
+            }
+        }
+
+        private static void ExtractQueryData(Dictionary<string, List<string>> fieldValue, string[] nameValue)
+        {
+            for (int i = 0; i < nameValue.Length; i++)
+            {
+                int equalSeparator = nameValue[i].IndexOf('=');
+                string name = ClearGarbage(nameValue[i].Substring(0, equalSeparator));
+                string value = ClearGarbage(nameValue[i].Substring(equalSeparator + 1));
+
+                if (!fieldValue.ContainsKey(name))
+                {
+                    fieldValue[name] = new List<string>();
+                }
+
+                fieldValue[name].Add(value);
+            }
+        }
+
+        private static string ClearGarbage(string dirty)
+        {
+            if (dirty.Contains('?'))
+            {
+                dirty = dirty.Substring(dirty.LastIndexOf('?') + 1);
+            }
+
+            return string.Join(" ", dirty
+                .Replace('+', ' ')
+                .Replace("%20", " ")
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim(" \t".ToCharArray())));
+        }
+    }
+}
