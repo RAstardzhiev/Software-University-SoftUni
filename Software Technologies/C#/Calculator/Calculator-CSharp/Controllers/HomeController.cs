8b261d222e69e1b0967865447fc545e34fbe2512
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-04-14 13:13:24 +0300

    Add Calculator to "Software Technologies\C#"

---
+++ Software Technologies/C#/Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Calculator_CSharp.Models;
+using System.Web.Mvc;
+using System;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "Reminder":
+                    result = calculator.LeftOperand % calculator.RightOperand;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Software Technologies/C#/Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+
+        public decimal LeftOperand { get; set; }
+
+        public decimal RightOperand { get; set; }
+
+        public string Operator { get; set; }
+
+        public decimal Result { get; set; }
+    }
+}
