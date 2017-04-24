cc966f05c86059d0f93a8b3941cc86022dfe4f15
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-04-24 11:06:06 +0300

    Add Delete comments functionality to "Software Technologies/C#/Blog/" and deleted a unusable view (used for tests)

--- Software Technologies/C#/Blog/Blog/Controllers/CommentController.cs
+++ Software Technologies/C#/Blog/Blog/Controllers/CommentController.cs
@@ -61,8 +61,13 @@ namespace Blog.Controllers
         // GET: Comment/Edit
         [Authorize]
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
             // Get comment from database
             var comment = new BlogDbContext().Comments
                 .Where(c => c.Id == id)
@@ -113,6 +118,79 @@ namespace Blog.Controllers
             }
 
             return View(model);
+        }
+
+        //
+        // GET: Comment/Delete
+        [Authorize]
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Get comment from database
+            var comment = new BlogDbContext().Comments
+                .Where(c => c.Id == id)
+                .FirstOrDefault();
+
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Check user permissions
+            if (!comment.AuthorId.Equals(User.Identity.GetUserId()) && !User.IsInRole("Admin"))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            // Create model
+            var model = new CommentViewModel();
+            model.Id = comment.Id;
+            model.ArticleId = comment.ArticleId;
+            model.Content = comment.Content;
+
+            // Return view
+            return View(model);
+        }
+
+        //
+        // POST: Comment/Delete
+        [Authorize]
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get comment from database
+                var comment = database.Comments
+                    .Where(c => c.Id == id)
+                    .FirstOrDefault();
+
+                // Check if comment exists
+                if (comment == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Get id of the article to which will return
+                int articleId = comment.ArticleId;
+
+                // Remove comment from database
+                database.Comments.Remove(comment);
+                database.SaveChanges();
+
+                return RedirectToAction("Details", "Article", new { id = articleId });
+            }
         }
     }
 }
