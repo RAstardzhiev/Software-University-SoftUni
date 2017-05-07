417376cb32459a194c954302a01e9c0ba00cbb83
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-07 22:59:05 +0300

    Add "C# Advanced/Sets and Dictionaries/12. Legendary Farming/" and "C# Advanced\Sets and Dictionaries\13. Srabsko Unleashed\" "

---
+++ C# Advanced/Sets and Dictionaries/12. Legendary Farming/LegendaryFarming.cs
@@ -0,0 +1,99 @@
+﻿namespace _12.Legendary_Farming
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LegendaryFarming
+    {
+        public static void Main()
+        {
+            var junkMaterials = new Dictionary<string, int>();
+            var keyMaterials = new Dictionary<string, int>();
+            keyMaterials["shards"] = 0;
+            keyMaterials["fragments"] = 0;
+            keyMaterials["motes"] = 0;
+
+            // Collect materials until reach the amount of 250 for some of the key materials
+            CollectMaterials(keyMaterials, junkMaterials);
+
+            // Print the legendary item
+            Console.WriteLine($"{ObtainLegendary(keyMaterials)} obtained!");
+
+            // Print remaining key mateirals
+            PrintMaterials(keyMaterials.OrderByDescending(m => m.Value).ThenBy(m => m.Key));
+
+            // Print collected junk materials
+            PrintMaterials(junkMaterials.OrderBy(m => m.Key));
+        }
+
+        private static void PrintMaterials(IOrderedEnumerable<KeyValuePair<string, int>> materials)
+        {
+
+            foreach (var material in materials)
+            {
+                Console.WriteLine($"{material.Key}: {material.Value}");
+            }
+        }
+
+        private static string ObtainLegendary(Dictionary<string, int> keyMaterials)
+        {
+            var materialName = keyMaterials
+                .Where(kvp => kvp.Value >= 250)
+                .First()
+                .Key;
+
+            // Remove 250 from the amount to obtain the legendary item
+            keyMaterials[materialName] -= 250;
+
+            // Find which is the legendary item obtained
+            switch (materialName)
+            {
+                case "shards":
+                    return "Shadowmourne";
+                case "fragments":
+                    return "Valanyr";
+                case "motes":
+                    return "Dragonwrath";
+                default:
+                    return "Big ERROR!!! Please call the developer. *** Big ERROR!!! *** Nothing";
+            }
+        }
+
+        private static void CollectMaterials(Dictionary<string, int> keyMaterials, Dictionary<string, int> junkMaterials)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine()
+                    .ToLower()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var quantity = int.Parse(input[i]);
+                    i++;
+                    var material = input[i];
+
+                    if (keyMaterials.ContainsKey(material))
+                    {
+                        keyMaterials[material] += quantity;
+
+                        if (keyMaterials[material] >= 250)
+                        {
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (!junkMaterials.ContainsKey(material))
+                        {
+                            junkMaterials[material] = 0;
+                        }
+
+                        junkMaterials[material] += quantity;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries/13. Srabsko Unleashed/SrabskoUnleashed.cs
@@ -0,0 +1,84 @@
+﻿namespace _13.Srabsko_Unleashed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SrabskoUnleashed
+    {
+        public static void Main()
+        {
+            var townSingerMoney = new Dictionary<string, Dictionary<string, long>>();
+
+            // Get Performance data from the console and assign values to the dictionary// Input Format: "singer @venue ticketsPrice ticketsCount"
+            while (true)
+            {
+                var performanceData = Console.ReadLine().Trim();
+                if (performanceData.ToLower() == "end")
+                {
+                    break;
+                }
+
+                // Validate input - Format: "singer @venue ticketsPrice ticketsCount"
+                var indexOfSeparation = performanceData.IndexOf(" @");
+                if (indexOfSeparation < 1)
+                {
+                    continue;
+                }
+
+                var indexOfTicketPrice = IndexOfTicketPrice(performanceData);
+                if (indexOfTicketPrice < 0 || performanceData[indexOfTicketPrice - 1] != ' ')
+                {
+                    continue;
+                }
+
+                var indexOfTicketsAmount = performanceData.LastIndexOf(' ');
+                if (indexOfTicketsAmount < indexOfTicketPrice + 1)
+                {
+                    continue;
+                }
+
+                // Parse input to dictionary
+                var singer = performanceData.Substring(0, indexOfSeparation);
+                var town = performanceData.Substring(indexOfSeparation + 2, indexOfTicketPrice - (indexOfSeparation + 2) - 1);
+                var priceOfATicket = long.Parse(performanceData.Substring(indexOfTicketPrice, indexOfTicketsAmount - indexOfTicketPrice).Trim());
+                var ticketsAmount = long.Parse(performanceData.Substring(indexOfTicketsAmount + 1));
+
+                if (!townSingerMoney.ContainsKey(town))
+                {
+                    townSingerMoney[town] = new Dictionary<string, long>();
+                }
+
+                if (!townSingerMoney[town].ContainsKey(singer))
+                {
+                    townSingerMoney[town][singer] = 0;
+                }
+
+                townSingerMoney[town][singer] += priceOfATicket * ticketsAmount;
+            }
+
+            // Print singers
+            foreach (var town in townSingerMoney)
+            {
+                Console.WriteLine(town.Key);
+                foreach (var singer in town.Value.OrderByDescending(kvp => kvp.Value))
+                {
+                    Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
+                }
+            }
+        }
+
+        private static int IndexOfTicketPrice(string performanceData)
+        {
+            for (int i = performanceData.Length - 1; i >= 6; i--)
+            {
+                if (char.IsLetter(performanceData[i]))
+                {
+                    return i + 2;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
