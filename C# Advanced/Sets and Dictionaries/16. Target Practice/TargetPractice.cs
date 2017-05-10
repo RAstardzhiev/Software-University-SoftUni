64a7b56fe4401efda49ea87d06a254678de549e0
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-10 16:39:36 +0300

    Add "C# Advanced\Sets and Dictionaries\16. Target Practice\" and "C# Advanced\Sets and Dictionaries\17. Lego Blocks\"

---
+++ C# Advanced/Sets and Dictionaries/16. Target Practice/TargetPractice.cs
@@ -0,0 +1,139 @@
+﻿namespace _16.Target_Practice
+{
+    using System;
+    using System.Linq;
+
+    public class TargetPractice
+    {
+        public static void Main()
+        {
+            var stairs = InitializeStairsMatrix();
+
+            // Fill the stairs matrix with snake symbols
+            GetSnakeOnStairs(stairs);
+
+            // Take a shot
+            ShotAtSnake(stairs);
+
+            // Preiint the resulting matrix
+            PrintMatrix(stairs);
+        }
+
+        private static void PrintMatrix(char[][] matrix)
+        {
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                Console.WriteLine(string.Join(string.Empty, matrix[row]));
+            }
+        }
+
+        private static void ShotAtSnake(char[][] matrix)
+        {
+            var shot = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var impactRow = shot[0];
+            var impactCol = shot[1];
+            var impactRadius = shot[2];
+
+            for (int row = matrix.Length - 1; row >= 0; row--)
+            {
+                for (int col = matrix[row].Length - 1; col >= 0; col--)
+                {
+                    if (IsCelInsideTheBlast(impactRow, impactCol, impactRadius, row, col))
+                    {
+                        matrix[row][col] = ' ';
+                    }
+                    else
+                    {
+                        FallDown(row, col, matrix);
+                    }
+                }
+            }
+        }
+
+        private static void FallDown(int row, int col, char[][] matrix)
+        {
+            var isNeededSwap = false;
+            var lastSpaceIndex = -1;
+
+            for (int i = row + 1; i < matrix.Length; i++)
+            {
+                if (matrix[i][col] == ' ')
+                {
+                    isNeededSwap = true;
+                    lastSpaceIndex = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (isNeededSwap)
+            {
+                matrix[lastSpaceIndex][col] = matrix[row][col];
+                matrix[row][col] = ' ';
+            }
+        }
+
+        private static bool IsCelInsideTheBlast(int impactRow, int impactCol, int impactRadius, int rowToCheck, int colToCheck)
+        {
+            var distance = Math.Sqrt((impactRow - rowToCheck) * (impactRow - rowToCheck) + (impactCol - colToCheck) * (impactCol - colToCheck));
+            return distance <= impactRadius;
+        }
+
+        private static void GetSnakeOnStairs(char[][] matrix)
+        {
+            var snake = Console.ReadLine().Replace(" ", string.Empty);
+            var snakeCharIndex = - 1;
+            var startRowAt = 'R';
+
+            for (int row = matrix.Length - 1; row >= 0; row--)
+            {
+                switch (startRowAt)
+                {
+                    case 'R': // Snake is climbing from right to left
+                        for (int col = matrix[row].Length - 1; col >= 0; col--)
+                        {
+                            snakeCharIndex = (snakeCharIndex == snake.Length - 1) ? 0 : snakeCharIndex + 1;
+                            matrix[row][col] = snake[snakeCharIndex];
+                        }
+
+                        startRowAt = 'L';
+                        break;
+                    case 'L': // Snake is climbing from left to right
+                        for (int col = 0; col < matrix[row].Length; col++)
+                        {
+                            snakeCharIndex = (snakeCharIndex == snake.Length - 1) ? 0 : snakeCharIndex + 1;
+                            matrix[row][col] = snake[snakeCharIndex];
+                        }
+
+                        startRowAt = 'R';
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static char[][] InitializeStairsMatrix()
+        {
+            var rowsCols = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = new char[rowsCols[0]][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new char[rowsCols[1]];
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries/17. Lego Blocks/LegoBlocks.cs
@@ -0,0 +1,72 @@
+﻿namespace _17.Lego_Blocks
+{
+    using System;
+    using System.Linq;
+
+    public class LegoBlocks
+    {
+        public static void Main()
+        {
+            var numberOfRows = int.Parse(Console.ReadLine());
+
+            var firstJaggedArray = GetJaggedArray(numberOfRows);
+            var secondJaggedArray = GetJaggedArray(numberOfRows);
+
+            if (IsFittingPossible(firstJaggedArray, secondJaggedArray))
+            {
+                for (int i = 0; i < firstJaggedArray.Length; i++)
+                {
+                    Console.WriteLine($"[{string.Join(", ", firstJaggedArray[i].Concat(secondJaggedArray[i].Reverse()))}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {TotalCellsCount(firstJaggedArray) + TotalCellsCount(secondJaggedArray)}");
+            }
+        }
+
+        private static int TotalCellsCount(int[][] jaggedArray)
+        {
+            var cellsCount = 0;
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                cellsCount += jaggedArray[i].Length;
+            }
+
+            return cellsCount;
+        }
+
+        private static bool IsFittingPossible(int[][] firstJaggedArray, int[][] secondJaggedArray)
+        {
+            var isFitted = true;
+
+            var upperRowsTotalCellsCount = firstJaggedArray[0].Length + secondJaggedArray[0].Length;
+
+            for (int i = 1; i < firstJaggedArray.Length; i++)
+            {
+                if (firstJaggedArray[i].Length + secondJaggedArray[i].Length != upperRowsTotalCellsCount)
+                {
+                    return false;
+                }
+            }
+
+            return isFitted;
+        }
+
+        private static int[][] GetJaggedArray(int numberOfRows)
+        {
+            var jaggedArray = new int[numberOfRows][];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                jaggedArray[i] = Console.ReadLine()
+                    .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            return jaggedArray;
+        }
+    }
+}
