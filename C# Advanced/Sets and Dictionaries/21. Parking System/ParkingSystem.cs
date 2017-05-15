8513cb6f0ddbdc3fa7ba477db58efeb8bb3ec1b0
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-15 23:14:26 +0300

    Add "C# Advanced\Sets and Dictionaries\21. Parking System\"

---
+++ C# Advanced/Sets and Dictionaries/21. Parking System/ParkingSystem.cs
@@ -0,0 +1,93 @@
+﻿namespace _21.Parking_System
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ParkingSystem
+    {
+        public static void Main()
+        {
+            var parkingDimensionsRolCol = InitializeParking();
+            var usedCells = new HashSet<Cell>();
+
+            var input = Console.ReadLine().Split();
+
+            while (input[0] != "stop")
+            {
+                var carEntranceRow = int.Parse(input[0]);
+                var carParkingArim = new Cell
+                {
+                    Row = int.Parse(input[1]),
+                    Column = int.Parse(input[2])
+                };
+
+                // Process car
+                if (IsCarParked(carParkingArim, usedCells, parkingDimensionsRolCol))
+                {
+                    // Print distance travelled to the park
+                    Console.WriteLine(Math.Abs((carEntranceRow + 1) - (carParkingArim.Row + 1)) + carParkingArim.Column + 1);
+                    usedCells.Add(carParkingArim);
+                }
+                else
+                {
+                    Console.WriteLine($"Row {carParkingArim.Row} full");
+                }
+
+                input = Console.ReadLine().Split();
+            }
+        }
+
+        private static bool IsCarParked(Cell carParkingArim, HashSet<Cell> usedCells, int[] parkingDimensions)
+        {
+            // Try park
+            if (usedCells.Where(c => c.Row == carParkingArim.Row && c.Column == carParkingArim.Column)
+                .FirstOrDefault() == null)
+            {
+                return true;
+            }
+
+            var testCol = 1;
+
+            // Loop around the row to find free cell to park
+            while (true)
+            {
+                var leftCol = carParkingArim.Column - testCol;
+                var rightCol = carParkingArim.Column + testCol;
+
+                if (leftCol <= 0 && rightCol >= parkingDimensions[1])
+                {
+                    break;
+                }
+
+                // Try park left
+                if (leftCol > 0 && 
+                    usedCells.Where(c => c.Row == carParkingArim.Row && c.Column == leftCol)
+                    .FirstOrDefault() == null)
+                {
+                    carParkingArim.Column = leftCol;
+                    return true;
+                }
+
+                // Try park right
+                if (rightCol < parkingDimensions[1] && 
+                    usedCells.Where(c => c.Row == carParkingArim.Row && c.Column == rightCol)
+                    .FirstOrDefault() == null)
+                {
+                    carParkingArim.Column = rightCol;
+                    return true;
+                }
+
+                testCol++;
+            }
+
+            return false;
+        }
+
+        private static int[] InitializeParking()
+        {
+            var dimmensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            return new int[] { dimmensions[0], dimmensions[1] };
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries/21. Parking System/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("21. Parking System")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("21. Parking System")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdd29c1b-b837-4dcd-910f-f8452800f20f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
