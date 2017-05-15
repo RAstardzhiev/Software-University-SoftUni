42b4b82b7b9d3d0a81fce38dd35505ce9765c9f2
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-15 09:46:18 +0300

    Add "C# Advanced\Sets and Dictionaries\20. The Heigan Dance\"

---
+++ C# Advanced/Sets and Dictionaries/20. The Heigan Dance/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace _20.The_Heigan_Dance
+{
+    public class Player
+    {
+        public int[] Position { get; set; }
+
+        public int Blood { get; set; }
+
+        public double Damage { get; set; }
+
+        public bool IsHitByCloud { get; set; }
+    }
+}
