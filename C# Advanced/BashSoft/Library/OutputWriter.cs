4f7008e6d2cd742f682ec2053b5bb5c97ac93410
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-17 13:31:27 +0300

    SETS AND DICTIONARIES LAB -> Add "C# Advanced\BashSoft\"

---
+++ C# Advanced/BashSoft/Library/OutputWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace Library
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class OutputWriter
+    {
+        public static void WriteMessageOnNewLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        // Display student
+        public static void PrintStudent(KeyValuePair<string, List<int>> student)
+        {
+            WriteMessageOnNewLine($"{student.Key} - {string.Join(", ", student.Value)}");
+        }
+    }
+}
---
+++ C# Advanced/BashSoft/StartUp/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace StartUp
+{
+    using Library;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            StudentsRepository.InitializeData();
+            StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+        }
+    }
+}
