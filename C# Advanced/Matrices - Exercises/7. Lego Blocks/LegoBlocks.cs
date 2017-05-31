9fa7d7bcefb871854ecbd903bd00e44df0d8e2a0
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-05-31 13:36:53 +0300

    Add "C# Advanced\Matrices - Exercises\7. Lego Blocks\" and "C# Advanced\Matrices - Exercises\8. Radioactive Mutant Vampire Bunnies\"

---
+++ C# Advanced/Matrices - Exercises/7. Lego Blocks/LegoBlocks.cs
@@ -0,0 +1,72 @@
+﻿namespace _7.Lego_Blocks
+{
+    using System;
+    using System.Linq;
+
+    public class LegoBlocks
+    {
+        public static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var firstMatrix = ReadInput(rows);
+            var secondMatrix = ReadInput(rows).Select(r => r.Reverse().ToArray()).ToArray();
+            PrintResult(firstMatrix, secondMatrix);
+        }
+
+        private static void PrintResult(int[][] firstMatrix, int[][] secondMatrix)
+        {
+            if (IsRectangularMatrix(firstMatrix, secondMatrix))
+            {
+                for (int i = 0; i < firstMatrix.Length; i++)
+                {
+                    Console.WriteLine($"[{string.Join(", ", firstMatrix[i].Concat(secondMatrix[i]))}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {CellsCount(firstMatrix, secondMatrix)}");
+            }
+        }
+
+        private static object CellsCount(int[][] firstMatrix, int[][] secondMatrix)
+        {
+            var numberOfCells = 0;
+
+            for (int i = 0; i < firstMatrix.Length; i++)
+            {
+                numberOfCells += firstMatrix[i].Length + secondMatrix[i].Length;
+            }
+
+            return numberOfCells;
+        }
+
+        private static bool IsRectangularMatrix(int[][] firstMatrix, int[][] secondMatrix)
+        {
+            for (int i = 1; i < firstMatrix.Length; i++)
+            {
+                if (firstMatrix[i].Length + secondMatrix[i].Length !=
+                    firstMatrix[i - 1].Length + secondMatrix[i - 1].Length)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int[][] ReadInput(int rows)
+        {
+            var matrix = new int[rows][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ C# Advanced/Matrices - Exercises/8. Radioactive Mutant Vampire Bunnies/RadioactiveBunnies.cs
@@ -0,0 +1,175 @@
+﻿namespace _8.Radioactive_Mutant_Vampire_Bunnies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RadioactiveBunnies
+    {
+        public static void Main()
+        {
+            int[] playerPosition;
+            var bunnies = InitializeMatrix(out playerPosition);
+            PlayTheGame(bunnies, playerPosition);
+        }
+
+        private static void PlayTheGame(bool[][] bunnies, int[] playerPosition)
+        {
+            while (true)
+            {
+                var playerSpepDirections = Console.ReadLine().ToCharArray();
+                var isPlayerEscaped = false;
+
+                foreach (var stepDirection in playerSpepDirections)
+                {
+                    isPlayerEscaped = IsPlayerEscaped(bunnies, playerPosition, stepDirection);
+                    MultiplyBunnies(bunnies);
+
+                    if (isPlayerEscaped)
+                    {
+                        PrintMatrix(bunnies);
+                        Console.WriteLine($"won: {playerPosition[0]} {playerPosition[1]}");
+                        return;
+                    }
+
+                    if (bunnies[playerPosition[0]][playerPosition[1]])
+                    {
+                        PrintMatrix(bunnies);
+                        Console.WriteLine($"dead: {playerPosition[0]} {playerPosition[1]}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(bool[][] bunnies)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < bunnies.Length; i++)
+            {
+                for (int j = 0; j < bunnies[i].Length; j++)
+                {
+                    sb.Append(bunnies[i][j] ? 'B' : '.');
+                }
+
+                sb.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+
+        private static void MultiplyBunnies(bool[][] bunnies)
+        {
+            var newBunnies = new Stack<int[]>();
+
+            for (int i = 0; i < bunnies.Length; i++)
+            {
+                for (int j = 0; j < bunnies[i].Length; j++)
+                {
+                    if (bunnies[i][j])
+                    {
+                        newBunnies.Push(new int[] { i + 1, j });
+                        newBunnies.Push(new int[] { i - 1, j });
+                        newBunnies.Push(new int[] { i, j + 1 });
+                        newBunnies.Push(new int[] { i, j - 1 });
+                    }
+                }
+            }
+
+            while (newBunnies.Count > 0)
+            {
+                var currentBunny = newBunnies.Pop();
+                if (IsInsideTheLayer(currentBunny, bunnies))
+                {
+                    bunnies[currentBunny[0]][currentBunny[1]] = true;
+                }
+            }
+        }
+
+        private static bool IsInsideTheLayer(int[] cell, bool[][] matrix)
+        {
+            return cell[0] >= 0 && cell[0] < matrix.Length && cell[1] >= 0 && cell[1] < matrix[0].Length;
+        }
+
+        private static bool IsPlayerEscaped(bool[][] bunnies, int[] playerPosition, char stepDirection)
+        {
+            switch (stepDirection)
+            {
+                case 'U': // Step Up
+                    playerPosition[0]--;
+
+                    if (!IsInsideTheLayer(playerPosition, bunnies))
+                    {
+                        playerPosition[0]++;
+                        return true;
+                    }
+
+                    break;
+                case 'D': // Step Down
+                    playerPosition[0]++;
+
+                    if (!IsInsideTheLayer(playerPosition, bunnies))
+                    {
+                        playerPosition[0]--;
+                        return true;
+                    }
+
+                    break;
+                case 'L': // Step Left
+                    playerPosition[1]--;
+
+                    if (!IsInsideTheLayer(playerPosition, bunnies))
+                    {
+                        playerPosition[1]++;
+                        return true;
+                    }
+
+                    break;
+                case 'R': // Step Right
+                    playerPosition[1]++;
+
+                    if (!IsInsideTheLayer(playerPosition, bunnies))
+                    {
+                        playerPosition[1]--;
+                        return true;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+        private static bool[][] InitializeMatrix(out int[] playerPosition)
+        {
+            var dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            playerPosition = new int[] { 0, 0 };
+            var bunnies = new bool[dimensions[0]][];
+
+            for (int i = 0; i < bunnies.Length; i++)
+            {
+                var input = Console.ReadLine();
+                bunnies[i] = new bool[input.Length];
+
+                for (int j = 0; j < bunnies[i].Length; j++)
+                {
+                    if (input[j] == 'B')
+                    {
+                        bunnies[i][j] = true;
+                    }
+                    else if (input[j] == 'P')
+                    {
+                        playerPosition[0] = i;
+                        playerPosition[1] = j;
+                    }
+                }
+            }
+
+            return bunnies;
+        }
+    }
+}
