e3e6969f4dd043285f1cc8798496ce27e72e3f88
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-06-07 18:29:31 +0300

    Update "C# Advanced\Manual String Processing - Exercises\"

---
+++ C# Advanced/Manual String Processing - Exercises/2. String Length vs2/StringLengthVs2.cs
@@ -0,0 +1,19 @@
+﻿namespace _2.String_Length_vs2
+{
+    using System;
+
+    public class StringLengthVs2
+    {
+        public static void Main()
+        {
+            var str = Console.ReadLine();
+
+            if (str.Length > 20)
+            {
+                str = str.Substring(0, 20);
+            }
+
+            Console.WriteLine($"{str.PadRight(20, '*')}");
+        }
+    }
+}
--- C# Advanced/Manual String Processing - Exercises/3. Formatting Numbers/FormattingNumbers.cs
+++ C# Advanced/Manual String Processing - Exercises/3. Formatting Numbers/FormattingNumbers.cs
@@ -6,18 +6,18 @@
     {
         public static void Main()
         {
-            string[] input = Console.ReadLine().Split("\n\r\t ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string[] input = Console.ReadLine().Split(new char[] { '\n', '\r', '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
             int a = int.Parse(input[0]);
             double b = double.Parse(input[1]);
             double c = double.Parse(input[2]);
 
-            string firstColInHex = $"|{a.ToString("X").PadRight(10, ' ')}";
-            string secondColBinary = $"|{Convert.ToString(a, 2).PadLeft(10, '0').Substring(0, 10)}";
-            string thirdCol = $"{b:F2}";
-            string fourthCol = $"{c:F3}";
+            string firstColInHex = $"|{a.ToString("X"), -10}";
+            string secondColBinary = $"|{Convert.ToString(a, 2).PadLeft(10, '0').Substring(0, 10)}|";
+            string thirdCol = $"{b, 10:F2}|";
+            string fourthCol = $"{c, -10:F3}|";
 
-            Console.WriteLine($"{firstColInHex}{secondColBinary}|{thirdCol.PadLeft(10, ' ')}|{fourthCol.PadRight(10, ' ')}|");
+            Console.WriteLine($"{firstColInHex}{secondColBinary}{thirdCol}{fourthCol}");
         }
     }
 }
