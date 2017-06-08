dc5d328b11de523b5ec0fecdc4d7e857a4be5738
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-06-08 11:13:38 +0300

    Begin with Game Mole Shooter -> "C# Advanced\Game\Mole Shooter\"

---
+++ C# Advanced/Game/Mole Shooter/Mole Shooter/CMole.cs
@@ -0,0 +1,43 @@
+﻿using Mole_Shooter.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mole_Shooter
+{
+    class CMole : CImageBase
+    {
+        private Rectangle _moleHotSpot = new Rectangle();
+
+        public CMole() : base(Resources.Mole)
+        {
+            _moleHotSpot.X = Left + 20;
+            _moleHotSpot.Y = Top - 1;
+            _moleHotSpot.Width = 30;
+            _moleHotSpot.Height = 40;
+        }
+
+        public void Update(int x, int y)
+        {
+            Left = x;
+            Top = y;
+            _moleHotSpot.X = Left + 20;
+            _moleHotSpot.Y = Top - 1;
+        }
+
+        public bool IsHit(int x, int y)
+        {
+            Rectangle cursor = new Rectangle(x, y, 1, 1); // Cursor Rectangle (quick way to check for hit)
+
+            if (_moleHotSpot.Contains(cursor))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# Advanced/Game/Mole Shooter/Mole Shooter/MoleShooter.cs
@@ -0,0 +1,58 @@
+﻿#define My_Debug
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mole_Shooter
+{
+    public partial class MoleShooter : Form
+    {
+#if My_Debug
+        int _cursX = 0;
+        int _cursY = 0;
+#endif
+        CMole _mole;
+
+        public MoleShooter()
+        {
+            InitializeComponent();
+
+            _mole = new CMole() { Left = 10, Top = 340 }; // Top Range 340-500
+        }
+
+        private void timerGameLoop_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+#if My_Debug
+            TextFormatFlags flags = TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+            Font _font = new System.Drawing.Font("Stencil", 12, FontStyle.Regular);
+            TextRenderer.DrawText(dc, "X=" + _cursX.ToString() + ":" + "Y=" + _cursY.ToString(), _font, 
+                new Rectangle(0, 0, 120, 20), SystemColors.ControlText, flags);
+#endif
+            _mole.DrowImage(dc);
+
+            base.OnPaint(e);
+        }
+
+        private void MoleShooter_MouseMove(object sender, MouseEventArgs e)
+        {
+#if My_Debug
+            _cursX = e.X;
+            _cursY = e.Y;
+#endif
+            this.Refresh();
+        }
+    }
+}
