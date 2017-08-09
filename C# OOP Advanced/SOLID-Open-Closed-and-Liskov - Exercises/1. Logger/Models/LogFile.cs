b42aa5772e90ff971cae6f903863f295f1d80ff5
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-08-09 23:41:55 +0300

    Update the way of calculating Size in "C# OOP Advanced/SOLID-Open-Closed-and-Liskov - Exercises/1. Logger/Models/LogFile.cs"

--- C# OOP Advanced/SOLID-Open-Closed-and-Liskov - Exercises/1. Logger/Models/LogFile.cs
+++ C# OOP Advanced/SOLID-Open-Closed-and-Liskov - Exercises/1. Logger/Models/LogFile.cs
@@ -1,8 +1,8 @@
 ﻿namespace _1.Logger.Models
 {
-    using System;
     using System.Text;
     using Interfaces;
+    using System.Linq;
 
     public class LogFile : ILogFile
     {
@@ -13,31 +13,15 @@
             this.sb = new StringBuilder();
         }
 
-        public int Size => this.CalculateSize();
+        public int Size { get; private set; }
 
         public void Write(string message)
         {
             this.sb.AppendLine(message);
-        }
 
-        private int CalculateSize()
-        {
-            var text = this.sb.ToString().Replace("<", string.Empty);
-            text = text.Replace("/", string.Empty);
-            text = text.Replace(">", string.Empty);
-
-            var size = 0;
-
-            for (int i = 0; i < this.sb.Length; i++)
-            {
-                if (sb[i] >= 'a' && sb[i] <= 'z' || 
-                    sb[i] >= 'A' && sb[i] <= 'Z')
-                {
-                    size += sb[i];
-                }
-            }
-
-            return size;
+            this.Size += message
+                .Where(c => char.IsLetter(c))
+                .Sum(c => c);
         }
 
         public override string ToString()
